8780b5b5534adead8babb62e9283e85163923b87
kenish-1BM18CS045 <53857041+kenish08@users.noreply.github.com>
2020-08-10 19:56:08 +0530

    Add files via upload

---
+++ c_2/ball.js
@@ -0,0 +1,15 @@
+const { easing, tween, styler } = window.popmotion;
+
+const divStyler = styler(document.querySelector('.box'));
+
+tween({
+  from: 0,
+  to: { x: 300, rotate: 180 },
+  duration: 1000,
+  ease: easing.backOut,
+  flip: Infinity,
+  // elapsed: 500,
+  // loop: 5,
+  // yoyo: 5
+}).start(divStyler.set);
+
---
+++ c_2/ball1.js
@@ -0,0 +1,7 @@
+tween({
+  from: 0,
+  to: { x: 300, rotate: 180 },
+  duration: 1000,
+  ease: easing.backOut,
+  flip: Infinity
+})
